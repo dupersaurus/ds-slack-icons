0f88fddac064e03c70cedd816a80f6894ddf7343
Jason DuPertuis <gallahad@me.com>
2017-06-27 22:26:36 -0400

    Send icons in_channel

--- server/pusheen.ts
+++ server/pusheen.ts
@@ -48,7 +48,7 @@ export class PusheenRouter {
     if (icon == null) {
       this.sendIconList(req, res);
     } else {
-      res.send(`{"text": "${req.body.text}", "attachments": [{"image-url": "${icon}"}]}`);
+      res.send(`{"response_type": "in_channel", "text": "${req.body.text}", "attachments": [{"image-url": "${icon}"}]}`);
     }
   }
 
